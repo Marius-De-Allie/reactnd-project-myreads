ebb7b53e116ebbf5f1d5364d44f27b4ba2c8776d
Marius De Allie <spectre4300s@gmail.com>
2020-02-25 15:59:25 -0500

    render jsx for bookshelf in FutureReadList functional component

--- src/components/FutureReadList.js
+++ src/components/FutureReadList.js
@@ -1,5 +1,17 @@
 import React from 'react';
 
 const FutureReadList = props => {
-
+  return (
+    <div className="list-books-content">
+      <div>
+        <div className="bookshelf">
+          <h2 className="bookshelf-title">Book Shelf Title</h2>
+          <div className="bookshelf-books">
+            <ol className="books-grid">
+            </ol>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
