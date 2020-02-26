8fe2e949f2cff0651cb969c5ad7b30729f1b45b9
Marius De Allie <spectre4300s@gmail.com>
2020-02-26 07:19:35 -0500

    Convert SearchInput functional component to class based component

--- src/components/SearchInput.js
+++ src/components/SearchInput.js
@@ -1,9 +1,14 @@
 import React from 'react';
 
-const SearchInput = props => (
-  <div className="search-books-input-wrapper">
-    <input type="text" placeholder="Search by title or author"/>
-  </div>
-);
+class SearchInput extends React.Component {
+  render() {
+    return (
+      <div className="search-books-input-wrapper">
+        <input type="text" placeholder="Search by title or author" value={}/>
+      </div>
+    );
+
+  }
+};
 
 export default SearchInput;
