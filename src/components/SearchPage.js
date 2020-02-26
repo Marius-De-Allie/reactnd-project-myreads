0d2f3af3ddd1fef5456e2cf789174c9f87623df7
Marius De Allie <spectre4300s@gmail.com>
2020-02-25 22:47:11 -0500

    Add SearchInput custom component to SearchPage component returned JSX

--- src/components/SearchPage.js
+++ src/components/SearchPage.js
@@ -5,7 +5,7 @@ import SearchInput from './SearchInput';
 const SearchPage = props => (
   <div className="search-books">
     <div className="search-books-bar">
-      SearchPage Comp
+      <SearchInput />
     </div>
   </div>
 );
