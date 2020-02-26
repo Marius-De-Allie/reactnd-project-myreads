60a7a1921a774decec23d2a021abb849be2147e3
Marius De Allie <spectre4300s@gmail.com>
2020-02-26 07:21:25 -0500

    Declare state object with a property of SearchTerm: '' within SearchInput class based component

--- src/components/SearchInput.js
+++ src/components/SearchInput.js
@@ -1,10 +1,13 @@
 import React from 'react';
 
 class SearchInput extends React.Component {
+  state = {
+    searchTerm: ''
+  };
   render() {
     return (
       <div className="search-books-input-wrapper">
-        <input type="text" placeholder="Search by title or author" value={}/>
+        <input type="text" placeholder="Search by title or author" />
       </div>
     );
 
