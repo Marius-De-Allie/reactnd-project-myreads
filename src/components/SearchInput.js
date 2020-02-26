970e9e2e7baa730927e87a3bc21e837c881fd750
Marius De Allie <spectre4300s@gmail.com>
2020-02-26 07:23:33 -0500

    Set value property on input element within SearchInput component to this.state.searchTerm

--- src/components/SearchInput.js
+++ src/components/SearchInput.js
@@ -7,7 +7,7 @@ class SearchInput extends React.Component {
   render() {
     return (
       <div className="search-books-input-wrapper">
-        <input type="text" placeholder="Search by title or author" />
+        <input type="text" placeholder="Search by title or author" value={this.state.searchTerm} />
       </div>
     );
 
