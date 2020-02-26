b574471ab0de8df546b971984f3e12c6fbc9d05d
Marius De Allie <spectre4300s@gmail.com>
2020-02-26 12:39:44 -0500

    Add onChange prop to input element within SearchInput component
    Add onChange prop to input element within SearchInput component, set prop value to this.search function

--- src/components/SearchInput.js
+++ src/components/SearchInput.js
@@ -14,7 +14,7 @@ class SearchInput extends React.Component {
   render() {
     return (
       <div className="search-books-input-wrapper">
-        <input type="text" placeholder="Search by title or author" value={this.state.searchTerm} />
+        <input type="text" placeholder="Search by title or author" value={this.state.searchTerm} onChange={this.search} />
       </div>
     );
 
