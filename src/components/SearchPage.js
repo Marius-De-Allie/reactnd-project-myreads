86acc69f462752b269d1fdc8e685fe3e3da473ee
Marius De Allie <spectre4300s@gmail.com>
2020-02-25 22:39:19 -0500

    Import SearchInput component into SearchPage.js file

--- src/components/SearchPage.js
+++ src/components/SearchPage.js
@@ -1,4 +1,6 @@
 import React from 'react';
+// IMPORT SEARCH INPUT COMPONENT INTO SEARCH PAGE
+import SearchInput from './SearchInput';
 
 const SearchPage = props => (
   <div className="search-books">
