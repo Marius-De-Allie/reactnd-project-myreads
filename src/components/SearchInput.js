e7d3d13e50e672287185cc6bc92a92ddce0a7514
Marius De Allie <spectre4300s@gmail.com>
2020-02-25 21:44:14 -0500

    Return search input jsx elements within SearchInput functional component

--- src/components/SearchInput.js
+++ src/components/SearchInput.js
@@ -1,5 +1,7 @@
 import React from 'react';
 
 const SearchInput = props => (
-  
+  <div className="search-books-input-wrapper">
+    <input type="text" placeholder="Search by title or author"/>
+  </div>
 );
