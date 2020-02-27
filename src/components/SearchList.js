8849de4f74b0d8b57f71f58aae4f65ffae3ccad7
Marius De Allie <spectre4300s@gmail.com>
2020-02-27 16:20:16 -0500

    Rename Booklist stateless functional component to SearchList in SearchList.js file

--- src/components/SearchList.js
+++ src/components/SearchList.js
@@ -2,7 +2,7 @@ import React from 'react';
 // IMPORT BOOK ITEM COMPONENT INTO BOOKLIST.
 import BookItem from './BookItem';
 
-const BookList = props => {
+const SearchList = props => {
   const book = props.searchResults.error !== 'empty query' ?  
     props.searchResults.map(book => 
     <BookItem 
