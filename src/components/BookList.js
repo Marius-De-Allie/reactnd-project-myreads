1c784f62337c8aa29d7dcb60ef8faf906e98bcfc
Marius De Allie <spectre4300s@gmail.com>
2020-02-26 20:31:45 -0500

    Update BookList component with a ternary operator to check whether props.searchResults exists before rendering the bookItems in the BookList

--- src/components/BookList.js
+++ src/components/BookList.js
@@ -2,14 +2,17 @@ import React from 'react';
 // IMPORT BOOK ITEM COMPONENT INTO BOOKLIST.
 import BookItem from './BookItem';
 
-const BookList = props => (
-  <div className={props.className}>
-    <ol className="books-grid">
-      {props.searchResults.map(book => {
-        return <BookItem {...book} />
-      })}
-    </ol>
-  </div>
-);
+const BookList = props => {
+  const book = props.searchResults ?  
+    props.searchResults.map(book => <BookItem />)
+    : <p>Please enter a valid search term to begin</p>;
+  return (
+    <div className={props.className}>
+      <ol className="books-grid">
+        {book}
+      </ol>
+    </div>
+  );
+};
 
 export default BookList;
