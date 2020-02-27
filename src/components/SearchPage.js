b14e9f5dc46c3030affcfec7526da0cc61c5ec3f
Marius De Allie <spectre4300s@gmail.com>
2020-02-27 16:34:57 -0500

    Update BookList component to Sreachlist custom component in SearchPage component's return statement

--- src/components/SearchPage.js
+++ src/components/SearchPage.js
@@ -9,7 +9,7 @@ const SearchPage = props => (
     <div className="search-books-bar">
       <SearchInput searchSubmit={props.searchSubmit} />
     </div>
-    <BookList 
+    <SearchList 
       className="search-books-results" 
       searchResults={props.searchResults}
     />
