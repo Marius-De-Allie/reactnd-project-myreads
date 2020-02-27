2f8b47a7387e65dd5869d65633003f90a4069a54
Marius De Allie <spectre4300s@gmail.com>
2020-02-27 18:48:29 -0500

    Return MainPage jsx elements within MainPage functional component

--- src/components/MainPage.js
+++ src/components/MainPage.js
@@ -1,5 +1,10 @@
 import React from 'react';
 
 const MainPage = props => {
-
+  return (
+    <div className="list-books">
+      <div className="list-books-content">
+      </div>
+    </div>
+  );
 };
