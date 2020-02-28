ebf51865d0fc6b84ea24161fcdf10ea3b1825ac5
Marius De Allie <spectre4300s@gmail.com>
2020-02-28 07:14:33 -0500

    Pass books property with value of this.state.books down to MainPage component within BooksApp component

--- src/App.js
+++ src/App.js
@@ -35,7 +35,9 @@ class BooksApp extends React.Component {
   render() {
     return (
       <div className="app">
-        <MainPage />
+        <MainPage 
+          books={this.state.books}
+        />
         {/*<SearchPage 
           searchSubmit={this.searchSubmit} 
           searchResults={this.state.searchResults} 
