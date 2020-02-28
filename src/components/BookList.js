80e6e266b65fb77923670fbce867e5821fe0c1ca
Marius De Allie <spectre4300s@gmail.com>
2020-02-28 08:24:56 -0500

    Call map method on props.currentlyReading array within BookList component, outputting BookItem component for each element in currentlyReading array

--- src/components/BookList.js
+++ src/components/BookList.js
@@ -2,6 +2,16 @@ import React from 'react';
 import BookItem from './BookItem';
 
 const BookList = props => {
+  let books;
+  if(props.currentlyReading !== undefined) {
+    books = props.currentlyReading.map(book => 
+    <BookItem 
+      key={book.id}
+      title={book.title}
+      author={book.authors}
+      imageURL={book.imageLinks.thumbnail}
+    />);
+  }
   return (
     <div className="bookshelf-books">
       <ol className="books-grid">
