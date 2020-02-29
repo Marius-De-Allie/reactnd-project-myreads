aba12f637f153d0ad3577ac59d408e06c4d210a8
Marius De Allie <spectre4300s@gmail.com>
2020-02-28 20:24:11 -0500

    Create futureReads prop on BookList component within FutureReadList component, setting prop value to props.futureBooks

--- src/components/FutureReadList.js
+++ src/components/FutureReadList.js
@@ -6,7 +6,7 @@ const FutureReadList = ({futureBooks}) => {
       <div>
         <div className="bookshelf">
           <h2 className="bookshelf-title">Book Shelf Title</h2>
-          <BookList />
+          <BookList futureReads={futureBooks} />
         </div>
       </div>
   );
