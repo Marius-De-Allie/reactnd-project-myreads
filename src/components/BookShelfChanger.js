a4bc3b0e1cd329edd55c52fe3affaed0f476e4be
Marius De Allie <spectre4300s@gmail.com>
2020-02-29 23:29:21 -0500

    Set value attribute on select element within BookShelfChanger component to value of props.shelf

--- src/components/BookShelfChanger.js
+++ src/components/BookShelfChanger.js
@@ -3,7 +3,7 @@ import React from 'react';
 const BookShelfChanger = props => {
   return (
     <div className="book-shelf-changer">
-      <select>
+      <select value={props.shelf}>
         <option value="move" disabled>Move to...</option>
         <option value="currentlyReading">Currently Reading</option>
         <option value="wantToRead">Want to Read</option>
