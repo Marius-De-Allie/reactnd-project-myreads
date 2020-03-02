51001e8de4e132b8240fb55a2f8a585bc3409f41
Marius-De-Allie <spectre4300s@yahoo.com>
2020-03-02 15:38:43 -0500

    Convert Searchlist functional component to class based component

--- src/components/SearchList.js
+++ src/components/SearchList.js
@@ -2,22 +2,30 @@ import React from 'react';
 // IMPORT BOOK ITEM COMPONENT INTO BOOKLIST.
 import BookItem from './BookItem';
 
-const SearchList = props => {
-  const book = props.searchResults.error !== 'empty query' ?  
-    props.searchResults.map(book => 
-    <BookItem 
-      title={book.title}
-      author={book.authors}
-      imageURL={book.imageLinks.thumbnail}
-    />)
-    : <p>No results please try another search term</p>;
-  return (
-    <div className={props.className}>
-      <ol className="books-grid">
-        {book}
-      </ol>
-    </div>
-  );
-};
+class SearchList extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return (
+      <div className={props.className}>
+        <ol className="books-grid">
+          {book}
+        </ol>
+      </div>
+    );
+  }
+}
+// = props => {
+//   const book = props.searchResults.error !== 'empty query' ?  
+//     props.searchResults.map(book => 
+//     <BookItem 
+//       title={book.title}
+//       author={book.authors}
+//       imageURL={book.imageLinks.thumbnail}
+//     />)
+//     : <p>No results please try another search term</p>;
+// };
 
 export default SearchList;
+
