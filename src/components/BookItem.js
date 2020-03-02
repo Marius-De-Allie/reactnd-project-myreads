e444b67ccdf4476d47f658b968620a723c056aab
Marius De Allie <spectre4300s@gmail.com>
2020-03-01 20:33:35 -0500

    Pass changeShelf prop down to BookShelfChanger component within BookItem component

--- src/components/BookItem.js
+++ src/components/BookItem.js
@@ -8,7 +8,10 @@ const BookItem = props => {
       <div className="book">
         <div className="book-top">
           <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url("${props.imageURL}")` }}></div>
-            <BookShelfChanger shelf={props.shelf} />
+            <BookShelfChanger 
+              shelf={props.shelf} 
+              changeShelf={props.changeShelf} 
+            />
           </div>
   <div className="book-title">{props.title}</div>
   <div className="book-authors">{props.author}</div>
