4fa081dc481660dbfcb842430a971bfd95c41ba6
Marius De Allie <spectre4300s@gmail.com>
2020-03-01 20:18:46 -0500

    Pass changeShelf prop down to BookList component within PrevReadList component

--- src/components/PrevReadList.js
+++ src/components/PrevReadList.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import BookList from './BookList';
 
-const PrevReadList = ({previousBooks}) => {
+const PrevReadList = ({previousBooks, changeShelf}) => {
   return (
       <div>
         <div className="bookshelf">
           <h2 className="bookshelf-title">Book Shelf Title</h2>
-          <BookList previousReads={previousBooks} />
+          <BookList previousReads={previousBooks} changeShelf={changeShelf} />
         </div>
       </div>
   );
