af2b64cd231e9aa2f0d745cb0451a36bea70dd2c
Marius-De-Allie <spectre4300s@yahoo.com>
2020-03-02 15:44:04 -0500

    Remove searchResults and showSearchPane properties from state object in BookApp class based component

--- src/App.js
+++ src/App.js
@@ -13,9 +13,7 @@ class BooksApp extends React.Component {
      * users can use the browser's back and forward buttons to navigate between
      * pages, as well as provide a good URL they can bookmark and share.
      */
-    books: [],
-    searchResults: [],
-    showSearchPae: false
+    books: []
   }
   // Function for making Async search request to BooksAPI.
   searchSubmit = async(query) => {
