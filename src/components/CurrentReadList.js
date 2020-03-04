e42182c7c44deff2025b0bf5677a0cf1c75d1232
Marius De Allie <spectre4300s@gmail.com>
2020-03-03 23:08:31 -0500

    Pass props.title into CurrentlyReadList component

--- src/components/CurrentReadList.js
+++ src/components/CurrentReadList.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import BookList from './BookList';
 
-const CurrentReadList = ({currentBooks, changeShelf}) => {
+const CurrentReadList = ({currentBooks, changeShelf, title}) => {
   console.log(currentBooks);
   return (
       <div>
         <div className="bookshelf">
-  <h2 className="bookshelf-title">Book Shelf Title</h2>
+  <h2 className="bookshelf-title">{title}</h2>
           <BookList currentlyReading={currentBooks} changeShelf={changeShelf} />
         </div>
       </div>
