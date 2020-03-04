7587ada8726a0e60b67d242a1baf2abd211953ff
Marius De Allie <spectre4300s@gmail.com>
2020-03-03 22:03:34 -0500

    Call props.history.push method within onShelfChange function in SearchBookShelfChanger component redirecting to the homepage '/'

--- src/components/SearchBookShelfChanger.js
+++ src/components/SearchBookShelfChanger.js
@@ -5,6 +5,8 @@ const SearchBookShelfChanger = props => {
   const onShelfChange = (evt) => {
     const shelfValue = evt.target.value;
     props.changeShelfSearch(props.result, shelfValue);
+    // Redirect to the main page.
+    props.history.push('/');
   };
   return (
     <div className="book-shelf-changer">
@@ -18,5 +20,5 @@ const SearchBookShelfChanger = props => {
     </div>
   );
 };
-
+// Call withRouter higher order function passing in this component so that we can access the History object of the component.
 export default withRouter(SearchBookShelfChanger);
