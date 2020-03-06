15185aace9297feaf7027e7b6ac5a1813e194d1c
Marius De Allie <spectre4300s@gmail.com>
2020-03-06 06:55:42 -0500

    Add Link component to NotFoundPage returned JSX, set to property on Link to a value of '/'

--- src/components/NotFoundPage.js
+++ src/components/NotFoundPage.js
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom';
 const NotFoundPage = () => (
   <div>
     <h1>404</h1>
-    <p>Page not Found</p>
+    <p>Page not Found<Link to="/"> Go Home</Link></p>
   </div>
 );
 
